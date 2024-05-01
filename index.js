50b73c29513ac6193427a37c961da8454b3b219f
ks2092003 <ks2092003@gmail.com>
2024-05-01 16:14:13 +0530

    first

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const wss = new WebSocketServer({ port: 3000 });
 const gamemanager = new gameManager();
 
 wss.on('connection', function connection(ws) {
-    console.log(wss)
     gamemanager.addUser(ws)
     ws.off("close",()=>{
         console.log("exit")
